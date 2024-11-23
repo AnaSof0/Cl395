f103f8064c037814516f6fa998636b2a99838da9
Ana <anasof@bu.edu>
2024-11-23 12:35:46 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 import NavBar from './components/NavBar';
 import './App.css';
 import Main from './pages/Main';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SE1 from './pages/SE1';
 import SE2 from './pages/SE2';
 import SE3 from './pages/SE3';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -14,9 +13,9 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/s1" element={<SE1/>}/>
-          <Route path="/s2" element={<SE2/>}/>
-          <Route path="/s3" element={<SE3/>}/>
+          <Route path="/s1" element={<SE1 />} />
+          <Route path="/s2" element={<SE2 />} />
+          <Route path="/s3" element={<SE3 />} />
         </Routes>
       </div>
     </Router>
@@ -24,4 +23,5 @@ function App() {
 }
 
 export default App;
+
 
